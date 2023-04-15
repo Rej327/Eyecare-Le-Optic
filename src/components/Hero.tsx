0d77b97c55022b205be6acb0df654b8e584f2eae
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-04-15 10:02:13 +0800

    Update bg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,10 +4,12 @@ import Button from "./Button";
 export default function Hero() {
   return (
     <div className={variable.heroContainer}>
-      <div className={variable.content}>
-        <p className={variable.title}>Eye Care Le Optique</p>
-        <p className={variable.subTitle}>Eye Clinic</p>
-        <Button text="Learn More" />
+      <div className={variable.bg}>
+        <div className={variable.content}>
+          <p className={variable.title}>Eye Care Le Optique</p>
+          <p className={variable.subTitle}>Eye Clinic</p>
+          <Button text="Learn More" />
+        </div>
       </div>
     </div>
   );
