3e6381e98e08e8fbda39b71c568d1f8da86a6205
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-04-20 12:32:50 +0800

    Our services ( adding masonry and fixing styles)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,44 @@
 import variable from "@/styles/variables.module.scss";
+import mason from "@/styles/masonry.module.scss";
+import Masonry from "react-masonry-css";
 import { STRING } from "../constant/String";
+import Image from "next/image";
+import serviceList from "@/assets/serviceList.jpg";
 
 const { servicesContent } = STRING;
 export default function Card() {
+  const breakpointColumnsObj = {
+    default: 3,
+    1024: 3,
+    834: 2,
+    414: 1,
+  };
+
   return (
     <div className={variable.sectionCard}>
-      <div className={variable.container}>
+      <p className={variable.head}>WHAT WILL DO?</p>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className={mason.myMasonryGrid}
+        columnClassName={mason.myMasonryGridColumn}
+      >
         {servicesContent.map((data) => (
-          <div className={variable.card}>
-            <p className={variable.title}>{data.title}</p>
-            <p className={variable.desc}>{data.desc}</p>
+          <div className={variable.cardContainer}>
+            <Image
+              src={serviceList}
+              width={0}
+              height={250}
+              alt="service"
+              className={variable.cardImage}
+            />
+            <div className={variable.card}>
+              <p className={variable.title}>{data.title}</p>
+              <p className={variable.desc}>{data.desc}</p>
+            </div>
           </div>
         ))}
-      </div>
+      </Masonry>
+      ;
     </div>
   );
 }
