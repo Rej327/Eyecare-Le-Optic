3ea5d9cacd09d761580f7ef5da27beff012e075b
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-04-27 12:00:35 +0800

    Update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,19 +9,19 @@ import Link from "next/link";
 const { heroTitle } = STRING;
 
 export default function Hero() {
-	return (
-		<div className={variable.heroContainer}>
-			{heroTitle.map((data) => (
-				<div className={variable.bg}>
-					<div className={variable.content}>
-						<p className={variable.title}>{data.title}</p>
-						<p className={variable.subTitle}>{data.subTitle}</p>
-						<Link href="#appointment">
-							<Button className={style.button} text={data.button} />
-						</Link>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className={variable.heroContainer}>
+      {heroTitle.map((data) => (
+        <div className={variable.bg}>
+          <div className={variable.content}>
+            <p className={variable.title}>{data.title}</p>
+            <p className={variable.subTitle}>{data.subTitle}</p>
+            <Link href="#appointment">
+              <Button className={style.button} text={data.button} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
