92ebc28a0224ea530c6f603e60d817f248e3990c
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-05-03 11:49:16 +0800

    Update

--- src/components/LamutClinic.tsx
+++ src/components/LamutClinic.tsx
@@ -9,43 +9,43 @@ import { STRING } from "../constant/String";
 const { clinicPhoto } = STRING;
 
 export default function LamutClinic() {
-	return (
-		<div className={variable.content}>
-			<p className={variable.header}>VISIT US</p>
-			<div className={variable.location}>
-				<Link href="https://goo.gl/maps/jPZHJTca8tKM1aZh6" target="_blank">
-					<Image
-						src={map}
-						alt="location"
-						height={350}
-						width={500}
-						className={variable.map}
-					/>
-				</Link>
-				<div className={variable.details}>
-					<p className={variable.town}>Lamut, Ifugao</p>
-					<p className={variable.address}>Rayodesol Lorem ipsum dolor</p>
-					<div className={variable.schedule}>
-						<p>Clinic Schedule</p>
-						<p>MONDAY to SATURDAY</p>
-						<p>From 8:00 AM to 6:00 PM</p>
-					</div>
-				</div>
-			</div>
-			<p className={variable.insideClinic}>INSIDE THE CLINIC</p>
-			<div className={variable.clinicPhotos}>
-				{clinicPhoto.map((data) => (
-					<div>
-						<Image
-							src={data.img}
-							alt="clinic"
-							height={300}
-							width={350}
-							className={variable.clinicImages}
-						/>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className={variable.content}>
+      <p className={variable.header}>VISIT US</p>
+      <div className={variable.location}>
+        <Link href="https://goo.gl/maps/jPZHJTca8tKM1aZh6" target="_blank">
+          <Image
+            src={map}
+            alt="location"
+            height={350}
+            width={500}
+            className={variable.map}
+          />
+        </Link>
+        <div className={variable.details}>
+          <p className={variable.town}>Lamut, Ifugao</p>
+          <p className={variable.address}>Rayodesol Lorem ipsum dolor</p>
+          <div className={variable.schedule}>
+            <p>Clinic Schedule</p>
+            <p>MONDAY to SATURDAY</p>
+            <p>From 8:00 AM to 6:00 PM</p>
+          </div>
+        </div>
+      </div>
+      <p className={variable.insideClinic}>INSIDE THE CLINIC</p>
+      <div className={variable.clinicPhotos}>
+        {clinicPhoto.map((data) => (
+          <div key={data.id}>
+            <Image
+              src={data.img}
+              alt="clinic"
+              height={300}
+              width={350}
+              className={variable.clinicImages}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
