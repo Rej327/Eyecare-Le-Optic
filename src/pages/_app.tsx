0cebd53f967dc1dc229300ef40396d670bd2f89a
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-05-03 12:26:00 +0800

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import "@/styles/variables.module.scss";
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
