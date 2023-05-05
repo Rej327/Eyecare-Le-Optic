a25c1293f628ddb381fa3f5c30df6915d95e441b
Yousefu24 <yousefu102419@gmail.com>
2023-05-05 18:53:55 +0800

    logo

--- src/components/NavOtherPage.tsx
+++ src/components/NavOtherPage.tsx
@@ -2,54 +2,54 @@
 
 import Image from "next/image";
 import variable from "../styles/nav.module.scss";
-import logoImg from "../assets/logo.jpg";
+import logoImg from "../assets/logo.png";
 import Link from "next/link";
 import Hamburger from "./HamburgerOtherPage";
 
 export default function NavOtherPage() {
-  return (
-    <div className={variable.navSection}>
-      <div className={variable.navigation}>
-        <Link href="/">
-          <div className={variable.logoContainer}>
-            <Image
-              src={logoImg}
-              className={variable.logoImage}
-              alt="logo"
-              height={30}
-              width={60}
-            />
-          </div>
-        </Link>
+	return (
+		<div className={variable.navSection}>
+			<div className={variable.navigation}>
+				<Link href="/">
+					<div className={variable.logoContainer}>
+						<Image
+							src={logoImg}
+							className={variable.logoImage}
+							alt="logo"
+							height={60}
+							width={130}
+						/>
+					</div>
+				</Link>
 
-        <div className={variable.sectionList}>
-          <p>
-            <Link href="/" id="style-2" data-replace="Home">
-              <span>Home</span>
-            </Link>
-          </p>
-          <p>
-            <Link href="/Our-Doctor" id="style-2" data-replace="Our Doctor">
-              <span>Our Doctor</span>
-            </Link>
-          </p>
-          <p>
-            <Link href="/AboutUs" id="style-2" data-replace="About Us">
-              <span>About Us</span>
-            </Link>
-          </p>
-          <p>
-            <Link
-              href="tel:0917583604"
-              id="style-2"
-              data-replace="(+63)-917-583-604"
-            >
-              <span>(+63)-917-583-604</span>
-            </Link>
-          </p>
-        </div>
-        <Hamburger />
-      </div>
-    </div>
-  );
+				<div className={variable.sectionList}>
+					<p>
+						<Link href="/" id="style-2" data-replace="Home">
+							<span>Home</span>
+						</Link>
+					</p>
+					<p>
+						<Link href="/Our-Doctor" id="style-2" data-replace="Our Doctor">
+							<span>Our Doctor</span>
+						</Link>
+					</p>
+					<p>
+						<Link href="/AboutUs" id="style-2" data-replace="About Us">
+							<span>About Us</span>
+						</Link>
+					</p>
+					<p>
+						<Link
+							href="tel:0917583604"
+							id="style-2"
+							data-replace="(+63)-917-583-604"
+						>
+							<span>(+63)-917-583-604</span>
+						</Link>
+					</p>
+				</div>
+				<Hamburger />
+			</div>
+		</div>
+	);
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,59 +2,59 @@
 
 import Image from "next/image";
 import variable from "../styles/variables.module.scss";
-import logoImg from "../assets/logo.jpg";
+import logoImg from "../assets/logo.png";
 import Hamburger from "./Hamburger";
 import Link from "next/link";
 
 export default function Navigation() {
-  return (
-    <div className={variable.navSection}>
-      <div className={variable.navigation}>
-        <div className={variable.logoContainer}>
-          <Link href="/">
-            <Image
-              src={logoImg}
-              className={variable.logoImage}
-              alt="logo"
-              height={30}
-              width={60}
-            />
-          </Link>
-        </div>
+	return (
+		<div className={variable.navSection}>
+			<div className={variable.navigation}>
+				<div className={variable.logoContainer}>
+					<Link href="/">
+						<Image
+							src={logoImg}
+							className={variable.logoImage}
+							alt="logo"
+							height={60}
+							width={130}
+						/>
+					</Link>
+				</div>
 
-        <div className={variable.sectionList}>
-          <p>
-            <Link href="/" id="style-2" data-replace="Home">
-              <span>Home</span>
-            </Link>
-          </p>
-          <p>
-            <Link href="/Our-Doctor" id="style-2" data-replace="Our Doctor">
-              <span>Our Doctor</span>
-            </Link>
-          </p>
-          <p>
-            <Link href="#our-clinic" id="style-2" data-replace="Our Clinic">
-              <span>Our Clinic</span>
-            </Link>
-          </p>
-          <p>
-            <Link href="/AboutUs" id="style-2" data-replace="About Us">
-              <span>About Us</span>
-            </Link>
-          </p>
-          <p>
-            <Link
-              href="tel:0917583604"
-              id="style-2"
-              data-replace="(+63)-917-583-604"
-            >
-              <span>(+63)-917-583-604</span>
-            </Link>
-          </p>
-        </div>
-        <Hamburger />
-      </div>
-    </div>
-  );
+				<div className={variable.sectionList}>
+					<p>
+						<Link href="/" id="style-2" data-replace="Home">
+							<span>Home</span>
+						</Link>
+					</p>
+					<p>
+						<Link href="/Our-Doctor" id="style-2" data-replace="Our Doctor">
+							<span>Our Doctor</span>
+						</Link>
+					</p>
+					<p>
+						<Link href="#our-clinic" id="style-2" data-replace="Our Clinic">
+							<span>Our Clinic</span>
+						</Link>
+					</p>
+					<p>
+						<Link href="/AboutUs" id="style-2" data-replace="About Us">
+							<span>About Us</span>
+						</Link>
+					</p>
+					<p>
+						<Link
+							href="tel:0917583604"
+							id="style-2"
+							data-replace="(+63)-917-583-604"
+						>
+							<span>(+63)-917-583-604</span>
+						</Link>
+					</p>
+				</div>
+				<Hamburger />
+			</div>
+		</div>
+	);
 }
